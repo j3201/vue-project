2325d4270ac55ad6b3d96659e084688c820dba79
3201 <780902395@qq.com>
2026-06-19 22:38:46 +0800

    deploy: vite base=/vue-project/, 路由切 hash 模式，适配 GitHub Pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import ImagePage from '../views/ImagePage.vue'
 import PdfPage from '../views/PdfPage.vue'
 import NavPage from '../views/NavPage.vue'
@@ -15,7 +15,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
