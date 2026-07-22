374a45041ad62b2fa69469f272d3fa0fc047e13b
3201 <780902395@qq.com>
2026-07-22 18:44:57 +0800

    精简项目：删除无用文件、测试代码、未使用依赖和配置

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+declare module 'file-saver' {
+  export function saveAs(data: Blob | File | string, filename?: string, options?: any): void
+  export const save: typeof saveAs
+  export default saveAs
+}
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
